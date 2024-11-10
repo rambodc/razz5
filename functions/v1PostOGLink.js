853cdf5cd21179fe78e7855653055836d55268ef
rambodc <rambodc@irobotx.io>
2024-11-09 19:21:37 -0700

    updated site fix OGLink

--- functions/v1PostOGLink.js
+++ functions/v1PostOGLink.js
@@ -1,14 +1,18 @@
 // Firebase Cloud Function: v1PostOGLink.js
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const { Storage } = require('@google-cloud/storage');
 
 // Only initialize Firebase Admin if it hasn't been initialized
 if (!admin.apps.length) {
     admin.initializeApp();
 }
 
+// Initialize Google Cloud Storage
+const storage = new Storage();
+
 module.exports.v1PostOGLink = functions.https.onRequest(async (req, res) => {
-    const postId = req.params[0];
+    const postId = req.params[0].replace('post/', '');
     if (!postId) {
         return res.status(400).send("Missing postId in the URL");
     }
@@ -18,12 +22,18 @@ module.exports.v1PostOGLink = functions.https.onRequest(async (req, res) => {
 
         // Retrieve the Open Graph storage URL based on postId
         const storageBucket = functions.config().storage.bucket;
-        const storageUrl = `https://firebasestorage.googleapis.com/v0/b/${storageBucket}/o/posts%2F${postId}%2Fog%2Findex.html?alt=media`;
+        const filePath = `posts/${postId}/og/index.html`;
+        const bucket = storage.bucket(storageBucket);
+        const file = bucket.file(filePath);
 
-        // Redirect to the storage URL
-        return res.redirect(301, storageUrl);
+        // Read the HTML content from the file
+        const [fileContent] = await file.download();
+
+        // Set appropriate headers to ensure the response is treated as HTML
+        res.set('Content-Type', 'text/html');
+        res.status(200).send(fileContent);
     } catch (error) {
         console.error(`Error in v1PostOGLink function for postId: ${postId}: ${error.message}`);
-        return res.status(500).send({ status: "error", message: error.message });
+        res.status(500).send({ status: "error", message: error.message });
     }
 });
